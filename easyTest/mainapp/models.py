ab84426d5a2b3333920878874e34f4b1a91dc385
ziphead <ymilto@gmail.com>
2018-10-21 15:45:19 +0300

    commenting

--- easyTest/mainapp/models.py
+++ easyTest/mainapp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
-# Create your models here.
-class Keyword(Core):
-    """Тэги""" 
-    keyword_name = models.CharField(_('kw-title'), max_length=60,  blank=False)
-    def str(self):
-        return self.keyword_name
+# # Create your models here.
+# class Keyword(Core):
+#     """Тэги""" 
+#     keyword_name = models.CharField(_('kw-title'), max_length=60,  blank=False)
+#     def str(self):
+#         return self.keyword_name
 
-    class Meta:
-        verbose_name = _('Тэг')
-        verbose_name_plural = _('Тэги)'
-        ordering = ('title')
+#     class Meta:
+#         verbose_name = _('Тэг')
+#         verbose_name_plural = _('Тэги)'
+#         ordering = ('title')
 
 
-class Question(Core):
-    """Вопрос """
-    question_text = models.CharField(_('qw_text'), max_length=500, blank=False)
-    answers  = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    keyword = models.ManyToManyField(Keyword)
+# class Question(Core):
+#     """Вопрос """
+#     question_text = models.CharField(_('qw_text'), max_length=500, blank=False)
+#     answers  = models.ForeignKey(Answer, on_delete=models.CASCADE)
+#     keyword = models.ManyToManyField(Keyword)
 
-    class Meta:
-        verbose_name = _('Вопрос')
-        verbose_name_plural = _('Вопросы')
+#     class Meta:
+#         verbose_name = _('Вопрос')
+#         verbose_name_plural = _('Вопросы')
 
 
