7c0a0b0144399f02837ac5aa2b1e9b3dbf6788ca
Evgeny <ku7nec0v@gmail.com>
2018-10-21 13:27:02 +0300

    добавил css, base.html, index.html, вью класс для главной страницы + внес изменения в файл настроек

--- easyTest/mainapp/views.py
+++ easyTest/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 
 # Create your views here.
+
+class MainView(TemplateView):
+	'''Класс отображает главную страницу'''
+	template_name = 'mainapp/index.html'
