3e597cf05377cc0320c2967f74b0c1d55960a635
Evgeny <ku7nec0v@gmail.com>
2018-10-22 18:03:44 +0300

    добавил страницу question_list + include + view

--- easyTest/mainapp/views.py
+++ easyTest/mainapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
+# from mainapp.models import Question
 
 # Create your views here.
 
 class MainView(TemplateView):
 	'''Класс отображает главную страницу'''
 	template_name = 'mainapp/index.html'
+
+
+'''
+после раскомментирования добавить в файл 'mainapp/urls.py' импорт этого класса и
+раскомментировать соответствующую строку в urlpatterns 
+'''
+# class QuestionListView(ListView):
+# 	'''Класс отображает страницу со всеми вопросами'''
+# 	model = Question
