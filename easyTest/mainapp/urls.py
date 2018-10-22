8b1a16f7dd06e90531c25ae41fddb6d6f4291291
Evgeny <ku7nec0v@gmail.com>
2018-10-22 22:14:16 +0300

    добавил класс для автоматического логина пользователей - test и admin, при нажатии на ссылки; добавил пользователей в админке root, test, admin

--- easyTest/mainapp/urls.py
+++ easyTest/mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from mainapp.views import  MainView
+from mainapp.views import  MainView, UsersRedirectView
 
 
 app_name = 'mainapp'
@@ -7,5 +7,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
     path('main/', MainView.as_view(), name='main'),
+    path('redirect/<slug:slug>/', UsersRedirectView.as_view(), name='redirect'),
     # path('questions/', QuestionListView.as_view(), name='questions'),
     ]
