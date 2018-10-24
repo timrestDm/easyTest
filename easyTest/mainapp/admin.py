22a3ed3aa8c6dfd5bc84bae1eb442164f7ffe86e
ziphead <ymilto@gmail.com>
2018-10-24 14:00:17 +0300

    123

--- easyTest/mainapp/admin.py
+++ easyTest/mainapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import Question,Answer,TestCategory,Test
 # Register your models here.
+# class KeywordAdmin(admin.ModelAdmin):
+#     fields = ('title', 'active', 'sort')
+# admin.site.register(Keyword,KeywordAdmin)
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'keyword', 'active', 'sort')
+    inlines = [
+        AnswerInline,
+    ]
+admin.site.register(Question,QuestionAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'question', 'is_correct', 'active', 'sort')
+
+admin.site.register(Answer,AnswerAdmin)
+
+# class TestCategoryAdmin(admin.ModelAdmin):
+#     fields = ('title', 'description', 'active', 'sort')
+# admin.site.register(TestCategory,TestCategoryAdmin)
+
+# class TestAdmin(admin.ModelAdmin):
+#     fields = ('title', 'description','keywords',  'category', 'max_questions', 'required_correct_answers', 'active', 'sort')
+# admin.site.register(Test,TestAdmin)
