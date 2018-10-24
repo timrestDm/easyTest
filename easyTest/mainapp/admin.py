514f33bba6ef20d8a4ecfd5ec0853fa6344c1382
amayro <aabmstu@gmail.com>
2018-10-24 19:23:14 +0300

    errors up

--- easyTest/mainapp/admin.py
+++ easyTest/mainapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Answer, TestCategory, Test
+from .models import *
 
 
 # class KeywordAdmin(admin.ModelAdmin):
@@ -12,26 +12,29 @@ class AnswerInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'keyword', 'active', 'sort')
-    inlines = [
-        AnswerInline,
-    ]
+    fields = ('description', 'active', 'sort')
+    inlines = [AnswerInline,]
 
 
 class AnswerAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'question', 'is_correct', 'active', 'sort')
+    fields = ('description', 'question', 'is_correct', 'active', 'sort')
 
 
 # class TestCategoryAdmin(admin.ModelAdmin):
 #     fields = ('title', 'description', 'active', 'sort')
 
 
-# class TestAdmin(admin.ModelAdmin):
-#     fields = ('title', 'description','keywords',  'category', 'max_questions', 'required_correct_answers', 'active', 'sort')
+class TestAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'owner', 'questions', 'keywords',  'category', 'max_questions',
+              'required_correct_answers', 'active', 'sort')
+
+class ResultAdmin(admin.ModelAdmin):
+    fields = ('owner', 'test', 'right_answers_count', 'wrong_answers_count', 'time',  'is_test_passed', 'active', 'sort')
 
 
 
-admin.site.register(Question,QuestionAdmin)
-admin.site.register(Answer,AnswerAdmin)
-# admin.site.register(Test,TestAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Result, ResultAdmin)
 # admin.site.register(TestCategory,TestCategoryAdmin)
--- easyTest/mainapp/urls.py
+++ easyTest/mainapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from mainapp.views import  MainView, UsersRedirectView
+import mainapp.views as mainapp
+from pathlib import PurePath
+import os
 
-
-app_name = 'mainapp'
+app_name = PurePath(os.path.dirname(__file__)).parts[-1]
 
 urlpatterns = [
-    path('', MainView.as_view(), name='main'),
-    path('main/', MainView.as_view(), name='main'),
-    path('redirect/<slug:slug>/', UsersRedirectView.as_view(), name='redirect'),
+    path('', mainapp.MainView.as_view(), name='main'),
+    path('redirect/<slug:slug>/', mainapp.UsersRedirectView.as_view(), name='redirect'),
+
+    path('test/<int:pk>', mainapp.QuestionList.as_view(), name='test'),
+    path('result/<int:pk>', mainapp.ResultDetail.as_view(), name='result'),
     # path('questions/', QuestionListView.as_view(), name='questions'),
     ]
