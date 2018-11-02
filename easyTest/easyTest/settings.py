d75b21b548b483eb64c96f0ebe8814e79fe876dd
amayro <aabmstu@gmail.com>
2018-11-02 19:09:09 +0300

    фикс настройик time zone для корректного подсчета времени

--- easyTest/easyTest/settings.py
+++ easyTest/easyTest/settings.py
@@ -108,10 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
