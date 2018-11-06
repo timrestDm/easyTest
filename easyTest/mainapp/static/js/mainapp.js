b4ef62a97ff5258935cd2b7d9bd5960a81cbd9fc
ESm1th <ku7nec0v@gmail.com>
2018-11-06 20:24:50 +0300

    добавил jquery и mainapp.js в папку static/mainapp/js

---
+++ easyTest/mainapp/static/js/mainapp.js
@@ -0,0 +1,35 @@
+
+$(function makeDisable() {
+    $('#submit_b').addClass('disabled').attr('disabled','disabled');
+});
+
+$(function testFunction() {
+    var [minutes, seconds] = $("#timer").text().split(":");
+    var myVar = setInterval(myFunction, 1000);
+    
+    function myFunction() {
+        if (minutes <= 0 && seconds <= 0) {
+            clearInterval(myVar);
+        } else if (minutes == 0 && seconds > 0) {
+            seconds = seconds - 1;
+            $("#timer").text(minutes + ":" + seconds);
+        } else if (minutes > 0  && seconds == '00') {
+            minutes = minutes - 1;
+            seconds = 60;
+            $("#timer").text(minutes + ":" + seconds);
+        } else if (minutes > 0  && seconds > 0) {
+            seconds = seconds - 1;
+            $("#timer").text(minutes + ":" + seconds);
+        };
+    };
+});
+
+$(function passTimeToSession() {
+    $("#submit_b").click(function () {
+        $("#left_time").attr('value', $("#timer").text());
+    });
+});
+
+
+
+    
