bba6dec9d8ccfd2e9f20b77632905385c373ceb8
ESm1th <ku7nec0v@gmail.com>
2018-11-12 19:32:14 +0300

    добавил формсет и формы вопроса и ответа

--- easyTest/mainapp/forms.py
+++ easyTest/mainapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from mainapp.models import Test, TestCategory
+from mainapp.models import Test, TestCategory, Question, Answer
 
 class TestForm(forms.ModelForm):
 	class Meta:
@@ -22,3 +22,30 @@ class TestCategoryForm(forms.ModelForm):
 			'description': _('Описание категории'),
 			'cat': _('Подкатегория'),
 		}
+
+class QuestionForm(forms.ModelForm):
+	class Meta:
+		model = Question
+		fields = ('description',)
+		labels = {
+			'description': _('Вопрос'),
+		}
+		widgets = {
+			'description': forms.Textarea(attrs={'cols':50, 'rows':2, 'placeholder': _('Введите текст вопроса')}),
+		}
+
+
+class AnswerForm(forms.ModelForm):
+	class Meta:
+		model = Answer
+		fields = ('description', 'is_correct')
+		labels = {
+			'description': _('Ответ'),
+			'is_correct': _('Верный'),
+		}
+		widgets = {
+			'description': forms.Textarea(attrs={'cols':50, 'rows':2, 'placeholder': _('Введите текст ответа')}),
+		}
+
+
+AnswerFormSet = forms.inlineformset_factory(Question, Answer, form=AnswerForm, can_delete=False, extra=6)
